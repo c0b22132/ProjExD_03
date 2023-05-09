d8613dfa4ec47af17d7be620b0dbcec44cf40fea
c0b221327d <c0b221327d@edu.teu.ac.jp>
2023-05-09 15:41:28 +0900

    練習問題３

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -49,6 +49,20 @@ class Bird:
             True, 
             False
         )
+        img0=pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"),0,2.0)
+        img1=pg.transform.flip(img0,True,False)
+        self._imgs={
+            (+1,0):img1,
+            (+1,-1):pg.transform.rotozoom(img1,45,1.0),
+            (0,-1):pg.transform.rotozoom(img1,90,1.0),
+            (-1,-1):pg.transform.rotozoom(img0,-45,1.0),
+            (-1,0):img0,
+            (-1,+1):pg.transform.rotozoom(img0,45,1.0),        
+            (0,+1):pg.transform.rotozoom(img1,-90,1.0),
+            (+1,+1):pg.transform.rotozoom(img1,-45,1.0),
+            (0,0):img1
+        }
+        self._img=self._imgs[(+1,0)]
         self._rct = self._img.get_rect()
         self._rct.center = xy
 
@@ -67,13 +81,17 @@ class Bird:
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
+        sum_mv=[0,0]
         for k, mv in __class__._delta.items():
             if key_lst[k]:
                 self._rct.move_ip(mv)
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
         if check_bound(screen.get_rect(), self._rct) != (True, True):
             for k, mv in __class__._delta.items():
                 if key_lst[k]:
                     self._rct.move_ip(-mv[0], -mv[1])
+        self._img=self._imgs[tuple(sum_mv)]
         screen.blit(self._img, self._rct)
 
 
